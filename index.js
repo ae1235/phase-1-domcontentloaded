c568a9d143e6dbbe02a8d71993cf0a6e03302db0
+ <+>
2022-06-13 22:31:22 +0300

    commit tree d8f5dc34494403ac449663d253e3af28b826869e

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateParagraphText();
+  
+  });
+  
+  function updateParagraphText() {
+    
+    const textToUpdate = document.querySelector("#text").textContent = "This is really cool!"
+  
+    return textToUpdate;
+  }
